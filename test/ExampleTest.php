9232698f6c64b5edb1c2d437809e725b0cd581ed
alexey slivkin <slivkin@jivosite.com>
2022-02-10 17:53:26 +0300

    init

---
+++ test/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test;
+
+final class ExampleTest extends \PHPUnit\Framework\TestCas
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
